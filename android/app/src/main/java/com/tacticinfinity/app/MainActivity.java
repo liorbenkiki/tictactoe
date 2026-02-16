57372de67a919fd200b513a2fb7f4cc3d8937e46
Lior Ben-kiki <104772534+lior-benkiki-simplisafe@users.noreply.github.com>
2026-02-16 11:07:45 -0500

    Feat: adnoird wrapper

---
+++ android/app/src/main/java/com/tacticinfinity/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tacticinfinity.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tacticinfinity.app',
+  appName: 'TacTicInfinity',
+  webDir: 'dist'
+};
+
+export default config;
